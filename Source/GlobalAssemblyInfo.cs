5d9f9254a916649fe20bf6a513dcf97ae0bb1470
Peter Hultqvist <phq@silentorbit.com>
2012-05-12 15:46:12 +0200

    Static version Easier to get started

---
+++ Source/GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("The Open Toolkit Library")]
+[assembly: AssemblyProduct("The Open Toolkit Library")]
+[assembly: AssemblyCopyright("Copyright © 2006 - 2012 the Open Toolkit Library")]
+[assembly: AssemblyTrademark("OpenTK")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.862.30254")]
